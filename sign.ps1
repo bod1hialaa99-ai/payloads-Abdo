2520f520666bbd37ca14fe04c872a0a0ed4cd95e
bod1hialaa99-ai <bod1hialaa99@gmail.com>
2026-02-12 17:50:50 +0800

    Update sign.ps1

--- sign.ps1
+++ sign.ps1
@@ -7,25 +7,19 @@ param(
     [string]$Server
 )
 
-# Import Active Directory module
-Import-Module ActiveDirectory -ErrorAction Stop
-
 # Build parameters
 $params = @{}
 if ($Server) { $params.Server = $Server }
 
 try {
-    # Get the domain object
-    $domain = Get-ADObject -Identity $DomainDN -Properties msDS-Other-Settings @params -ErrorAction Stop
-
-    # Extract the msDS-Other-Settings attribute
-    $settings = $domain.'msDS-Other-Settings'
+    # Get the domain object using AD module cmdlets
+    $domain = Get-ADObject -Identity $DomainDN -Properties "msDS-Other-Settings" @params -ErrorAction Stop
 
     # Check if LDAP signing is required (value "2")
     $ldapSigningRequired = $false
-    if ($settings) {
-        foreach ($line in $settings) {
-            if ($line -match 'LDAPServerIntegrity:\s*2') {
+    if ($domain.'msDS-Other-Settings') {
+        foreach ($setting in $domain.'msDS-Other-Settings') {
+            if ($setting -match 'LDAPServerIntegrity:\s*2') {
                 $ldapSigningRequired = $true
                 break
             }
